51feb93487a51921eeb787405896fa226e31732d
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-20 00:38:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   
   counter = 0
-  while array[counter] != value_to_find
+  while array[counter] != value_to_find do
   
-  counter += 1
+  counter ++ 1
 end
   
   return array[value_to_find]
